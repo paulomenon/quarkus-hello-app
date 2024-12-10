7c305a79db7d383016a8e03bc7650a68de0b35b1
pamenon <pamenon@redhat.com>
2024-12-10 22:31:05 +0000

    Fixed uncessary files

--- src/test/java/org/pamenon/GreetingEventIT.java
+++
@@ -1,8 +0,0 @@
-package org.pamenon;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-class GreetingEventIT extends GreetingEventTest {
-    // Execute the same tests but in packaged mode.
-}
